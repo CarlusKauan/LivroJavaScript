2ad40c1bd3a83c283fb44a4442237c0b1d41c32f
carloskauan <carloskauan190@gmail.com>
2022-03-25 15:00:43 -0300

    Calculo do valor do jantar

---
+++ cap1/ex_9/script.js
@@ -0,0 +1,5 @@
+var jantar = Number(prompt("Digite o valor do janta ?"));
+var taxa = jantar * 0.10;
+var valor = jantar + taxa;
+
+alert("Taxa do Garçom R$: " + taxa.toFixed(2) + "\nTotal a Pagar R$:" + valor.toFixed(2)) 
