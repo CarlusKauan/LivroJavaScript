e8ad10778a3c17f3da2bedb5961b38576be98ccb
carloskauan <carloskauan190@gmail.com>
2022-03-25 14:59:23 -0300

    Calculo dobro de um numero

---
+++ cap1/ex_6/script.js
@@ -0,0 +1,9 @@
+
+// entrada de dados com 'prompt'
+var myname = prompt("Qual o seu nome ?");
+var mynumber = prompt("Qual o seu número ?");
+var double = mynumber * 2
+alert("O dobro do seu número " + myname + " é: " + double);
+console.log(double);
+
+
