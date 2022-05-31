dc040482ec43f2225842ad4219a4e19478b3182d
carloskauan <carloskauan190@gmail.com>
2022-05-31 14:41:54 -0300

    Gerador de E-mail !

--- cap6/ex_5/app.js
+++ cap6/ex_5/app.js
@@ -1,1 +1,32 @@
-console.log(':)') 
+
+const $inFuncionario = document.getElementById('inFuncionario')
+const $outEmail = document.getElementById('outEmail')
+const $btGeral = document.getElementById('btGerar')
+
+const Gerar = () => {
+    let nome = $inFuncionario.value
+
+    if(nome == "" || nome.indexOf(" ") == -1){
+        alert('Nome invalido, digite o nome completo !')
+        $inFuncionario.focus()
+        return
+    }
+    console.log(nome)
+
+    let partes = nome.split(" ");
+    let email = "";
+    let tam = partes.length;
+
+    // percorre itens do vetor (exceto o ultimo)
+    for(var i = 0; i < tam - 1; i++){
+        email += partes[i].charAt(0);
+    }
+
+    // concatena as letras iniciais com a última parte
+    email += partes[tam - 1] + "@empresa.com.br";
+
+    // exibe e-mail
+    $outEmail.textContent = "E-mail: " + email.toLowerCase();
+};
+
+$btGeral.addEventListener('click', Gerar)
